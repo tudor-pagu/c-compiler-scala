74ca4c9e0588a0fe1dbd57d42ab7b2afc4f4a6b6
tudor <pagu.tudor@gmail.com>
2025-09-27 11:50:40 +0200

    add primary expression

--- src/main/scala/parser/Expressions.scala
+++ src/main/scala/parser/Expressions.scala
@@ -13,23 +13,17 @@ def unaryExpression: ParseRule[AstNodeExt] = ???
 def secondaryExpression: ParseRule[AstNodeExt] = ???
 
 def primaryExpressionMonadic: ParseRule[AstNodeExt] = {
-  def buildBinary(left: AstNodeExt, ops: List[(TokenInfo, AstNodeExt)]): Either[CompilerError, AstNodeExt] = {
-    ops.foldLeft[Either[CompilerError, AstNodeExt]](Right(left)) { case (acc, (opToken, right)) =>
-      acc match {
-        case Left(_) => acc
-        case Right(acc) =>       opToken.token match {
-        case Token.Plus => Right(AstNodeExt.Binary(BinaryOp.Add, acc, right))
-        case Token.Minus => Right(AstNodeExt.Binary(BinaryOp.Sub, acc, right))
-        case _ => Left(CompilerError("Invalid operator", opToken.span))
-      }
-
-      }
-
-    }
-  }
-  
   for {
     left <- secondaryExpression
     rest <- (OneOf(List(Token.Plus, Token.Minus)) <*> secondaryExpression).many
-  } yield buildBinary(left, rest)
+  } yield rest.foldLeft[Either[CompilerError, AstNodeExt]](Right(left)) {
+    case (acc, (opToken, right)) => acc match
+    case Left(_) => acc
+    case Right(acc) => opToken.token match {
+      case Token.Plus => Right(AstNodeExt.Binary(BinaryOp.Add, acc, right))
+      case Token.Minus => Right(AstNodeExt.Binary(BinaryOp.Sub, acc, right))
+      case _ => Left(CompilerError("Invalid binary operation", opToken.span))
+    }
+
+  }
 }
