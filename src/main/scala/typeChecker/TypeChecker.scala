4823082bb6709ff688498f665e72795bfdcbef7d
tudor <pagu.tudor@gmail.com>
2025-09-28 23:25:12 +0200

    add type checker, didnt test it yet

--- src/main/scala/typeChecker/TypeChecker.scala
+++ src/main/scala/typeChecker/TypeChecker.scala
@@ -9,11 +9,14 @@ import tpagu.compiler.Span
 import tpagu.compiler.parser.PostfixOp
 import tpagu.compiler.parser.DeclarationSpecifier
 import tpagu.compiler.parser.Declarator
+import tpagu.compiler.parser.DirectDeclarator
+import tpagu.compiler.parser.Declaration
 
 type TypeEnvironment = Map[String, Type]
 
 def createError(message: String, span: Span): CompilerError =
   CompilerError(message, span)
+
 def lookupType(identifier: AstExt, env: TypeEnvironment): Type =
   identifier.node match {
     case AstExtKind.Identifier(name) =>
@@ -27,70 +30,155 @@ def lookupType(identifier: AstExt, env: TypeEnvironment): Type =
   }
 
 object TypeCheck {
-  def typeOf(e: AstExt, nv: TypeEnvironment): Type = e.node match {
-    case AstExtKind.IntLiteral(_) => NumT(8)
-    case AstExtKind.Binary(op, l, r) =>
-      (op, typeOf(l, nv), typeOf(r, nv)) match {
-        case (
-              BinaryOp.Add | BinaryOp.Sub | BinaryOp.Mult | BinaryOp.Div,
-              l @ NumT(lsize, lsigned),
-              r @ NumT(rsize, rsigned)
-            ) => {
-          // TODO: This works now when the only type is int, but will not be correct once we have more types.
-          l
+  def typeOf(e: AstExt, nv: TypeEnvironment): (Type, TypeEnvironment) =
+    e.node match {
+      case AstExtKind.IntLiteral(_) => (NumT(8), nv)
+      case AstExtKind.Binary(op, l, r) =>
+        (op, typeOf(l, nv)._1, typeOf(r, nv)._1) match {
+          case (
+                BinaryOp.Add | BinaryOp.Sub | BinaryOp.Mult | BinaryOp.Div,
+                l @ NumT(lsize, lsigned),
+                r @ NumT(rsize, rsigned)
+              ) => {
+            // TODO: This works now when the only type is int, but will not be correct once we have more types.
+            (l, nv)
+          }
+          case _ =>
+            throw createError(
+              s"Invalid binary operation: ${op.toString} between ${typeOf(l, nv).toString} and ${typeOf(r, nv).toString}",
+              e.span
+            )
         }
-        case _ =>
+      case AstExtKind.PrefixOperation(op, e) =>
+        (
+          (op, typeOf(e, nv)._1) match {
+            case (PrefixOp.Negation, t @ NumT(size, signed)) =>
+              Type.numericalPromotion(t)
+            case (PrefixOp.UnaryPlus, t @ NumT(size, signed)) =>
+              Type.numericalPromotion(t)
+            case (_, t) =>
+              throw createError(
+                s"Invalid unary opration on ${t.toString}",
+                e.span
+              )
+          },
+          nv
+        )
+      case AstExtKind.PostfixOperation(op, e) =>
+        (
+          (op, typeOf(e, nv)._1) match {
+            case (
+                  PostfixOp.FunctionCall(args),
+                  t @ FunT(returnType, params)
+                ) => {
+              if (args.size != params.size) {
+                throw createError(
+                  s"Tried to call a function with {args.size} arguments, when the function has ${params.size} parameters",
+                  e.span
+                )
+              }
+              val argTypes = args.map(typeOf(_, nv))
+              if (argTypes != params) {
+                throw createError(
+                  s"Mismatch between types expected by function and arguments.",
+                  e.span
+                )
+              }
+              returnType
+            }
+            case _ =>
+              throw createError(s"Tried to call non-function type.", e.span)
+          },
+          nv
+        )
+      case AstExtKind.Identifier(name) => (lookupType(e, nv), nv)
+      case AstExtKind.DeclarationList(declSpecifiers, initDeclaratorList) => {
+        var newNv = nv;
+        for ((declarator, init) <- initDeclaratorList) {
+          val declaredType = getTypeOfDeclaration(declSpecifiers, declarator)
+          val name = getNameOfDeclarator(declarator).getOrElse(
+            throw createError(
+              "Abstract declaration not allowed here. You must give this declaration a name.",
+              e.span
+            )
+          )
+          newNv = nv + (name -> declaredType)
+        }
+        (NoneT(), newNv)
+      }
+      case AstExtKind.FunctionDefinition(declaration, body) => {
+        val declarationType = getTypeOfDeclaration(declaration)
+        val name = getNameOfDeclarator(declaration.declarator).getOrElse(
           throw createError(
-            s"Invalid binary operation: ${op.toString} between ${typeOf(l, nv).toString} and ${typeOf(r, nv).toString}",
+            "Abstract declaration not allowed here. You must give this declaration a name.",
             e.span
           )
-      }
-    case AstExtKind.PrefixOperation(op, e) =>
-      (op, typeOf(e, nv)) match {
-        case (PrefixOp.Negation, t @ NumT(size, signed)) =>
-          Type.numericalPromotion(t)
-        case (PrefixOp.UnaryPlus, t @ NumT(size, signed)) =>
-          Type.numericalPromotion(t)
-        case (_, t) =>
-          throw createError(s"Invalid unary opration on ${t.toString}", e.span)
-      }
-    case AstExtKind.PostfixOperation(op, e) =>
-      (op, typeOf(e, nv)) match {
-        case (PostfixOp.FunctionCall(args), t @ FunT(returnType, params)) => {
-          if (args.size != params.size) {
+        )
+        declarationType match {
+          case FunT(_, _) => {}
+          case t =>
             throw createError(
-              s"Tried to call a function with {args.size} arguments, when the function has ${params.size} parameters",
+              s"Expected function type in function declaration, but got ${t}. This is probably a bug in the parser.",
               e.span
             )
-          }
-          val argTypes = args.map(typeOf(_, nv))
-          if (argTypes != params) {
-            throw createError(
-              s"Mismatch between types expected by function and arguments.",
-              e.span
-            )
-          }
-          returnType
         }
-        case _ => throw createError(s"Tried to call non-function type.", e.span)
+        val newNv = nv + (name -> declarationType)
+        (NoneT(), newNv)
       }
-    case AstExtKind.Identifier(name) => lookupType(e, nv)
-    case AstExtKind.DeclarationList(declSpecifiers, initDeclaratorList)  => {
-      for ( (declarator, init) <- initDeclaratorList) {
-        val declaredType = getTypeOfDeclaration(declSpecifiers, declarator)
+      case AstExtKind.Block(statements) => {
+        (
+          NoneT(),
+          statements.foldLeft[TypeEnvironment](nv) { (accNv, statement) =>
+            typeOf(statement, accNv)._2
+          }
+        )
       }
-      NoneT()
+      case AstExtKind.TranslationUnit(statements) => {
+        (
+          NoneT(),
+          statements.foldLeft[TypeEnvironment](nv) { (accNv, statement) =>
+            typeOf(statement, accNv)._2
+          }
+        )
+      }
+      case AstExtKind.ExprStatement(e: AstExt) => {
+        // compute the type of the expresssion just to check it.
+        val typeOfE = typeOf(e, nv)
+        (NoneT(), nv)
+      }
+      case AstExtKind.Nothing => (NoneT(), nv)
+      case AstExtKind.Return(e) => {
+        val typeOfE = typeOf(e, nv)
+        (NoneT(), nv)
+      }
     }
-    case AstExtKind.FunctionDefinition(_, _) => NoneT()
-    case AstExtKind.Block(_) => NoneT()
 
+}
 
+def getBaseType(declSpecifiers: List[DeclarationSpecifier]): Type = ???
+
+def getTypeOfDeclaration(declaration: Declaration): Type = getTypeOfDeclaration(
+  declaration.declarationSpecifiers,
+  declaration.declarator
+)
+
+def getTypeOfDeclaration(
+    declSpecifiers: List[DeclarationSpecifier],
+    declarator: Declarator
+): Type = {
+  val baseType = getBaseType(declSpecifiers)
+  declarator.direct match {
+    case DirectDeclarator.Variable(_) => baseType
+    case DirectDeclarator.Function(_, params) =>
+      FunT(baseType, params.map(getTypeOfDeclaration(_)))
+    case DirectDeclarator.InnerDeclarator(decl) =>
+      getTypeOfDeclaration(declSpecifiers, decl)
   }
-
 }
 
-
-def getTypeOfDeclaration(declSpecifiers: List[DeclarationSpecifier], declarator: Declarator): Type = {
-  
-  ???
-}
+def getNameOfDeclarator(declarator: Declarator): Option[String] =
+  declarator.direct match {
+    case DirectDeclarator.Variable(name)        => name
+    case DirectDeclarator.Function(name, _)     => name
+    case DirectDeclarator.InnerDeclarator(decl) => getNameOfDeclarator(decl)
+  }
