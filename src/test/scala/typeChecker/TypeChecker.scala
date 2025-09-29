29515416779b9f091411bee338b545f596b04ad1
tudor <pagu.tudor@gmail.com>
2025-09-29 15:05:18 +0200

    tested type checker pretty thoroughly. I think type checker + parser is mostly correct now

--- src/test/scala/typeChecker/TypeChecker.scala
+++ src/test/scala/typeChecker/TypeChecker.scala
@@ -65,114 +65,114 @@ class TypeCheckerTest extends FunSuite {
 
   def testResultingType(input: String, expected: String) =
     assertEquals(parseExpression(input)._1.toString(), expected)
-  //
-  // test("simple type checking") {
-  //   testResultingType("2 + 2", "NumT(4,true)")
-  // }
-  //
-  // test("simple type checking") {
-  //   testResultingType("2 - 2", "NumT(4,true)")
-  // }
-  //
-  // test("failure 1") {
-  //   expectTypeError(
-  //     """
-  //     int main() {
-  //       f(2,3,5);
-  //     }
-  //     """,
-  //     "Undefined identifier: f"
-  //   )
-  // }
-  //
-  // test("function introduces parameters into type environment") {
-  //   expectTypePass("""
-  //     int f(int a, int b) {
-  //       return a + b;
-  //     }
-  //     """)
-  // }
-  //
-  // test("argument correct pass 1") {
-  //   expectTypePass("""
-  //     int f(int a, int b) {
-  //       return a + b;
-  //     }
-  //     int main() {
-  //       f(2, 3);
-  //     }
-  //     """)
-  // }
-  //
-  // test("argument mismatch failure 1") {
-  //   expectTypeError(
-  //     """
-  //     int f(int a, int b) {
-  //       return a + b;
-  //     }
-  //     int main() {
-  //       f(2, 3, 4);
-  //     }
-  //     """,
-  //     "Tried to call a function with 3 arguments, when the function takes 2 parameters"
-  //   )
-  // }
-  //
-  // test("argument mismatch failure 2") {
-  //   expectTypeError(
-  //     """
-  //     int f(int a, int b) {
-  //       return a + b;
-  //     }
-  //     int main() {
-  //       f();
-  //     }
-  //     """,
-  //     "Tried to call a function with 0 arguments, when the function takes 2 parameters"
-  //   )
-  // }
-  //
-  // test("argument mismatch failure 3") {
-  //   expectTypeError(
-  //     """
-  //     int f() {
-  //       return 5;
-  //     }
-  //     int main() {
-  //       f(2);
-  //     }
-  //     """,
-  //     "Tried to call a function with 1 arguments, when the function takes 0 parameters"
-  //   )
-  // }
-  //
-  // test("argument mismatch failure 4") {
-  //   expectTypeError(
-  //     """
-  //     int f() {
-  //       return 5;
-  //     }
-  //     int main() {
-  //       int x = f(2);
-  //     }
-  //     """,
-  //     "Tried to call a function with 1 arguments, when the function takes 0 parameters"
-  //   )
-  // }
-  //
-  // test("declared type mismatch failure") {
-  //   expectTypeError(
-  //     """
-  //     int f() {
-  //       return 5;
-  //     }
-  //     int main() {
-  //       int x(int, int) = f(2);
-  //     }
-  //     """,
-  //     "Tried to call a function with 1 arguments, when the function takes 0 parameters"
-  //   )
-  // }
+
+  test("simple type checking") {
+    testResultingType("2 + 2", "NumT(4,true)")
+  }
+
+  test("simple type checking") {
+    testResultingType("2 - 2", "NumT(4,true)")
+  }
+
+  test("failure 1") {
+    expectTypeError(
+      """
+      int main() {
+        f(2,3,5);
+      }
+      """,
+      "Undefined identifier: f"
+    )
+  }
+
+  test("function introduces parameters into type environment") {
+    expectTypePass("""
+      int f(int a, int b) {
+        return a + b;
+      }
+      """)
+  }
+
+  test("argument correct pass 1") {
+    expectTypePass("""
+      int f(int a, int b) {
+        return a + b;
+      }
+      int main() {
+        f(2, 3);
+      }
+      """)
+  }
+
+  test("argument mismatch failure 1") {
+    expectTypeError(
+      """
+      int f(int a, int b) {
+        return a + b;
+      }
+      int main() {
+        f(2, 3, 4);
+      }
+      """,
+      "Tried to call a function with 3 arguments, when the function takes 2 parameters"
+    )
+  }
+
+  test("argument mismatch failure 2") {
+    expectTypeError(
+      """
+      int f(int a, int b) {
+        return a + b;
+      }
+      int main() {
+        f();
+      }
+      """,
+      "Tried to call a function with 0 arguments, when the function takes 2 parameters"
+    )
+  }
+
+  test("argument mismatch failure 3") {
+    expectTypeError(
+      """
+      int f() {
+        return 5;
+      }
+      int main() {
+        f(2);
+      }
+      """,
+      "Tried to call a function with 1 arguments, when the function takes 0 parameters"
+    )
+  }
+
+  test("argument mismatch failure 4") {
+    expectTypeError(
+      """
+      int f() {
+        return 5;
+      }
+      int main() {
+        int x = f(2);
+      }
+      """,
+      "Tried to call a function with 1 arguments, when the function takes 0 parameters"
+    )
+  }
+
+  test("declared type mismatch failure") {
+    expectTypeError(
+      """
+      int f() {
+        return 5;
+      }
+      int main() {
+        int x(int, int) = f(2);
+      }
+      """,
+      "Tried to call a function with 1 arguments, when the function takes 0 parameters"
+    )
+  }
   
   test("Complex call") {
     expectTypePass(
@@ -189,17 +189,98 @@ class TypeCheckerTest extends FunSuite {
       """)
   }
 
-  // test("correct function call 1") {
-  //   expectTypePass(
-  //     """
-  //     int f() {
-  //       return 5;
-  //     }
-  //     int main() {
-  //       f();
-  //     }
-  //     """
-  //   )
-  // }
+  test("Complex call fail") {
+    expectTypeError(
+      """
+      int f() {
+        return 5;
+      }
+      int c(int a, int f) {
+        return a + f + 2;
+      }
+      int main() {
+        int y = c(f(2), c(2, 3));
+      }
+      """, "")
+  }
+
+  test("Complex call 2") {
+    expectTypePass(
+      """
+      int f() {
+        return f() + 5;
+      }
+      int c(int a, int f) {
+        return a + f + 2;
+      }
+      int main() {
+        int y = c(f(), c(c(4,5), 3));
+        int x = c(y, y);
+      }
+      """)
+  }
+
+  test("Complex call failure 2") {
+    expectTypeError(
+      """
+      int f() {
+        return f() + 5;
+      }
+      int c(int a, int f) {
+        return a + f + 2;
+      }
+      int main() {
+        int y = c(f(), c(c(4,5), 3));
+        int x = c(c, y);
+      }
+      """, "")
+  }
+  
+  test("Complex call failure 3") {
+    expectTypeError(
+      """
+      int f(int a) {
+        return a + 2;
+      }
+      int g(int a) {
+        return a;
+      }
+
+      int main() {
+        int x = g(f);
+      }
+      """, "")
+  }
+
+  test("Inner declarations shadow outer ones.") {
+    expectTypePass(
+      """
+      int f(int a) {
+        return a + 2;
+      }
+      int g(int a) {
+        return a;
+      }
+
+      int main() {
+        int f = 5;
+        int x = g(f);
+      }
+      """)
+  }
+
+
+  test("correct function call 1") {
+    expectTypePass(
+      """
+      int f() {
+        return 5;
+      }
+      int main() {
+        f();
+      }
+      """
+    )
+  }
 
 }
