693d53c63f115f8bcc70a30329a39ddd2b606ae3
tudor <pagu.tudor@gmail.com>
2025-10-03 20:48:09 +0200

    desugarer initially implemented

--- src/main/scala/desugar/AstCore.scala
+++ src/main/scala/desugar/AstCore.scala
@@ -1,17 +1,26 @@
 package tpagu.compiler.desugar
 import tpagu.compiler.typeChecker.Type
 
-sealed trait AstC {}
+sealed trait AstC {
+}
 
+// An expression can also double as a statement in the core AST.
+// So if we encounter an expression at the first level inside of a block,
+// we assume it was actually an expression statement. e.g ( `f(2,3);` or `2 + 3;` )
+// Otherwise it would have been caught in the parser or type checker.
 case class IntLiteral(value: Int, t: Type) extends AstC
-case class Add(l:IntLiteral, r: IntLiteral, t: Type) extends AstC
-case class Mult(l:IntLiteral, r: IntLiteral, t: Type) extends AstC
-case class Neg(e: IntLiteral, t: Type) extends AstC
+case class Add(l:AstC , r:AstC , t: Type) extends AstC
+case class Mult(l:AstC, r:AstC, t: Type) extends AstC
+case class Neg(e:AstC, t: Type) extends AstC
 case class FunctionCall(callee: AstC, args: List[AstC], t: Type) extends AstC
 case class Identifier(name: String, t: Type) extends AstC
 
 case class Declaration(name: String,t: Type, init:Option[AstC]) extends AstC
-case class FunctionDefinition(name: String, paramNames:List[String], t: Type) extends AstC // t tells us the types of the params, while params tells us the names
+case class FunctionDefinition(name: String, paramNames:List[String], body:AstC, t: Type) extends AstC // t tells us the types of the params, while params tells us the names
 case class Block(statements: List[AstC]) extends AstC
 case class TranslationUnit(statements: List[AstC]) extends AstC
-case class Return(e: AstC)
+case class Return(e: AstC) extends AstC
+case class Cast(e: AstC, newType: Type) extends AstC
+// first execute this, then execute that. Both statements are executed
+// in the same block/scope.
+case class Seq(statements: List[AstC]) extends AstC
--- src/main/scala/desugar/Desugar.scala
+++ src/main/scala/desugar/Desugar.scala
@@ -1,6 +1,61 @@
 package tpagu.compiler.desugar
 
-// object Desugar {
-//   def desugar(e: AstExt)
-//
-// }
+import java.util.IdentityHashMap
+import tpagu.compiler.parser.AstExt
+import tpagu.compiler.typeChecker.Type
+import tpagu.compiler.parser.AstExtKind
+import java.util.function.BinaryOperator
+import tpagu.compiler.parser.BinaryOp
+import tpagu.compiler.parser.PrefixOp
+import tpagu.compiler.parser.PostfixOp
+import tpagu.compiler.typeChecker.getTypeOfDeclaration
+import tpagu.compiler.Span
+import tpagu.compiler.parser.AstExtKind.ExprStatement
+
+object Desugar {
+  // the typeMap is produced by the TypeChecker as it type checks a program.
+  // It will cache the type of every AstExt it encounters so that we can use it in the
+  // desugarer
+  def desugar(e: AstExt)(implicit typeMap: IdentityHashMap[AstExt, Type]): AstC = {
+    e.node match {
+      case AstExtKind.IntLiteral(num) => IntLiteral(num, typeMap.get(e))
+      case AstExtKind.Binary(op, l, r) =>
+        op match {
+          case BinaryOp.Add => Add(desugar(l), desugar(r), typeMap.get(e))
+          case BinaryOp.Sub => Add(desugar(l), Neg(desugar(r), typeMap.get(r)), typeMap.get(e))
+          // TODO add more
+          case _ => ???
+        }
+      case AstExtKind.PrefixOperation(op, inner) => 
+        op match {
+          case PrefixOp.Negation => Neg(desugar(inner), typeMap.get(e))
+          case PrefixOp.UnaryPlus => Cast(desugar(inner), typeMap.get(e))
+        }
+      case AstExtKind.PostfixOperation(op, inner) => op match {
+        case PostfixOp.FunctionCall(args) => FunctionCall(desugar(inner), args.map(arg=>desugar(arg)), typeMap.get(e))
+      }
+      case AstExtKind.Identifier(name) => Identifier(name, typeMap.get(e))
+      case AstExtKind.DeclarationList(declSpecifiers, initDeclaratorList) => Seq(initDeclaratorList.map(initDecl => {
+          implicit val span: Span = e.span // at this point, we should *never* get a compiler error when deducing this type, since it got typechecked.
+          val declaredType = getTypeOfDeclaration(declSpecifiers, initDecl._1)
+          Declaration(initDecl._1.direct.getName().get, declaredType, initDecl._2.map(i => Desugar.desugar(i)))
+        }
+      ))
+      case AstExtKind.FunctionDefinition(declaration, body) => {
+        declaration.declarator.direct match {
+          case tpagu.compiler.parser.DirectDeclarator.Function(name, params) => {
+            FunctionDefinition(name.get, params.map(param => param.declarator.direct.getName().get), desugar(body), typeMap.get(e))
+          }
+          case _ => throw RuntimeException("Didnt get function definition in desugarer when it was expected.")
+        }
+      }
+
+      case AstExtKind.Block(statements) => Block(statements.map(desugar(_)))
+      case AstExtKind.TranslationUnit(statements) => TranslationUnit(statements.map(desugar(_)))
+      case AstExtKind.ExprStatement(e) => desugar(e)
+      case AstExtKind.Nothing => throw RuntimeException("tried to desugar Nothing, this should get filtered out ahead of time.")
+      case AstExtKind.Return(e) => Return(desugar(e))
+    }
+  }
+
+}
